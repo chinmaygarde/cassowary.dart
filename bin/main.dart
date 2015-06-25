a712651b1e2e72a58186b543023d4e82a4516307
Chinmay Garde <chinmaygarde@google.com>
2015-06-24 18:18:58 -0700

    Avoid exposing internal classes from the cassowary library

--- bin/main.dart
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
-
-import 'package:cassowary/cassowary.dart' as cassowary;
-
-main(List<String> arguments) {
-  print('Hello world');
-}
